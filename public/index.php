8322b2ebe341693c21af257119d9f974f9577973
mohamed.yousef <engineer.mohamed.yossef@gmail.com>
2026-01-04 15:13:01 +0400

    feat: update the index.php to handle the static assets in case of using the PHP built-in server

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
 <?php
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
+
+// Handle static assets in case of running PHP built-in server
+if (php_sapi_name() == 'cli-server') {
+    if (file_exists(__DIR__ . $_SERVER['REQUEST_URI']) &&
+        !is_dir(__DIR__ . $_SERVER['REQUEST_URI'])
+    ) {
+        return false;
+    }
+}
 
 // Create new app
 $app = new \SigmaPHP\Core\App\Kernel();
