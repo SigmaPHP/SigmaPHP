11f9300d40cf2d4ca20b0b4f16c22efc113edcd2
mohamed.yousef <engineer.mohamed.yossef@gmail.com>
2026-02-01 10:39:17 +0400

    In this commit:     1- Remove the dirty static assets handler     2- Add 'allow_http_method_override' and 'static_assets_route' options to app config file

--- public/index.php
+++ public/index.php
@@ -2,15 +2,6 @@
 
 require 'vendor/autoload.php';
 
-// Handle static assets in case of running PHP built-in server
-if (php_sapi_name() == 'cli-server') {
-    if (file_exists(__DIR__ . $_SERVER['REQUEST_URI']) &&
-        !is_dir(__DIR__ . $_SERVER['REQUEST_URI'])
-    ) {
-        return false;
-    }
-}
-
 // Create new app
 $app = new \SigmaPHP\Core\App\Kernel();
 
